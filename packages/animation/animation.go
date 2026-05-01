64fefc80e2644f0e6d866beb4e63612294461829
gojur laptop <gogata.199999@gmail.com>
2026-05-01 21:29:24 +0300

    Ported Animation and utility easing

---
+++ packages/animation/animation.go
@@ -0,0 +1,60 @@
+package animation
+
+import (
+	"big-black-box/packages/utility/number"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+type Animation struct {
+	ItemCount           int
+	ItemsPerSecond      float32
+	IsLooping, IsPaused bool
+
+	Time float32
+}
+
+func NewAnimation(itemCount int, itemsPerSecond float32, loop bool) Animation {
+	return Animation{ItemCount: itemCount, ItemsPerSecond: itemsPerSecond, IsLooping: loop}
+}
+
+//=================================================================
+
+func (a *Animation) Update() {
+	if !a.IsPaused {
+		a.Time += 1 / float32(ebiten.TPS())
+	}
+
+	var duration = a.Duration()
+	if a.Time >= duration {
+		if a.IsLooping {
+			a.Time = 0
+		} else {
+			a.Time = duration
+		}
+	}
+}
+
+func (a *Animation) SetDuration(seconds float32) {
+	a.ItemsPerSecond = float32(a.ItemCount) / seconds
+}
+func (a *Animation) SetIndex(index int) {
+	index = number.Limit(index, 0, a.ItemCount-1)
+	a.Time = number.Map(float32(index), 0, float32(a.ItemCount), 0, a.Duration())
+}
+
+//=================================================================
+
+func (a *Animation) Index() int {
+	return int(number.Map(a.Time, 0, a.Duration(), 0, float32(a.ItemCount)))
+}
+func (a *Animation) Duration() float32 {
+	return float32(a.ItemCount) / a.ItemsPerSecond
+}
+
+func (a *Animation) IsFinished() bool {
+	return a.Time == a.Duration()
+}
+func (a *Animation) IsPlaying() bool {
+	return !a.IsFinished() && !a.IsPaused
+}
